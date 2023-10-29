3e0c85eac0f04652b775b7e4050d3044bc3d84b6
harshrupapara <hrupapara@horizontal.com>
2023-10-29 16:12:31 +0530

    banner-done

--- src/components/authorable/General/Banner/Banner.tsx
+++ src/components/authorable/General/Banner/Banner.tsx
@@ -1,22 +1,23 @@
 import { Link } from '@sitecore-jss/sitecore-jss-nextjs';
 import RichTextA11yWrapper from '@/components/helpers/RichTextA11yWrapper/RichTextA11yWrapper';
 import React from 'react';
+import { ChakravyuhFeature } from '.generated/models/Feature.ChakravyuhFeature.model';
 
-export default function Banner(props: any) {
-  const propData = props?.fields?.data?.datasource;
-  const richTitleValue = propData?.title?.value;
-  const rteSubValue = propData?.description?.value;
-  const backgroundImageURL = `${
-    process.env.NEXT_PUBLIC_HOST_URL + propData?.bannerImage?.jsonValue?.value?.src
-  }`;
-  console.log('banner', backgroundImageURL);
+export type BannerProp = ChakravyuhFeature.Fields.Banner;
+export default function Banner(props: BannerProp) {
+  console.log('props', props?.fields);
+  const propData = props?.fields;
+  const richTitleValue = propData?.Title?.value;
+  const rteSubValue = propData?.Description?.value;
+  const backgroundImageURL = `${propData?.PageImage?.value?.src}`;
+  console.log('banner', propData?.CTA?.value);
   return (
     <section className="mb-8 md:mb-24">
       <div
         className="relative overflow-hidden bg-cover bg-no-repeat h-[500px] md:h-[780px] "
         style={{
           backgroundPosition: '50%',
-          backgroundImage: `url(${backgroundImageURL})`,
+          backgroundImage: `url(${process.env.NEXT_PUBLIC_HOST_URL + backgroundImageURL})`,
         }}
       >
         <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsla(0,0%,0%,0.75)] bg-fixed">
@@ -30,7 +31,7 @@ export default function Banner(props: any) {
               </div>
               <Link
                 className="rounded border-2 border-neutral-50 px-[46px] py-[14px] text-sm md:text-xl font-semibold uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-100 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200"
-                field={propData?.cta?.jsonValue}
+                field={propData?.CTA?.value}
               />
             </div>
           </div>
